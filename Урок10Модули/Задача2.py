00918c514d0aea1c34cea42e0debefd7a8dcbba6
3D23 <dmitrichenkov.dima@mail.ru>
2022-10-12 20:25:32 +0300

    CommitTimeAndRandom

---
+++ Урок10Модули/Задача2.py
@@ -0,0 +1,9 @@
+from random import randint
+
+sbornaya1 = []
+sbornaya2 = []
+count1 = int(input('Введите количество участников в первой сборной: '))
+count2 = int(input('Введите количество участников во второй сборной: '))
+sport1 = randint(1, count1)
+sport2 = randint(1, count2)
+print('Пловец ',sport1,' - ','Пловец ',sport2)
